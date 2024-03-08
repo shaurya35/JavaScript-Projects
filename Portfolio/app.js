21ce38791b03ac5c0741a30866e68dd2556ce494
shaurya35 <shauryajha35@gmail.com>
2024-03-09 01:21:33 +0530

    added responsiveness for mobiles

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -20,6 +20,7 @@ let currMode = "light";
 mode.addEventListener("click",() => {
   if(currMode === "light"){
     currMode = "dark";
+    currMode2 = "dark";
     document.querySelector("body").style.backgroundColor = "#232946";
     document.querySelector(".header").style.backgroundColor = "#232946";
     document.querySelector(".main").style.color = "#b8c1ec";
@@ -33,6 +34,7 @@ mode.addEventListener("click",() => {
   }
   else{
     currMode = "light";
+    currMode2 = "light";
     document.querySelector("body").style.backgroundColor = "#fbeaeb";document.querySelector(".header").style.backgroundColor = "#2f3c7e";
     document.querySelector(".left-hello").style.color = "#f52549";
     document.querySelector(".main").style.color = "black";
@@ -44,5 +46,81 @@ mode.addEventListener("click",() => {
     document.querySelector(".outer-button").innerHTML = "Light Mode";
   }
 });
+
+/* Menu Bar */
+
+let header = document.querySelector("header");
+let openMenu = document.querySelector(".menu-bar");
+let closeMenu = document.querySelector(".menu-bar-rev");
+
+openMenu.addEventListener("click",() => {
+  header.classList.add("open");
+});
+
+closeMenu.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+
+let home = document.querySelector(".home1");
+let skills = document.querySelector(".skills1");
+let projects = document.querySelector(".projects1");
+let contacts = document.querySelector(".contacts1");
+let light = document.querySelector(".light1");
+let sidebar = document.querySelector(".sidebar");
+home.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+skills.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+projects.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+contacts.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+light.addEventListener("click", () => {
+  header.classList.remove("open");
+});
+
+/*light mode from menu */
+
+let mode2 = document.querySelector(".light1");
+let currMode2 = "light";
+
+mode2.addEventListener("click",() => {
+  if(currMode2 === "light"){
+    currMode2 = "dark";
+    document.querySelector("body").style.backgroundColor = "#232946";
+    document.querySelector(".header").style.backgroundColor = "#232946";
+    document.querySelector(".main").style.color = "#b8c1ec";
+    document.querySelector(".left-hello").style.color = "#fffffe";
+    document.querySelector(".quote").style.color = "#b8c1ec";
+    document.querySelector("hr").style.backgroundColor = "#fffffe";
+    document.querySelector(".skills").style.color = "#eebbc3";
+    document.querySelector(".headingProjects").style.color = "#eebbc3";
+    document.querySelector("footer").style.backgroundColor = "#232946";
+    document.querySelector(".outer-button").innerHTML = "Dark Mode";
+    currMode = "dark";
+    document.querySelector(".light1").innerHTML = "Dark Mode";
+  }
+  else{
+    currMode2 = "light";
+    document.querySelector("body").style.backgroundColor = "#fbeaeb";
+    document.querySelector(".header").style.backgroundColor = "#2f3c7e";
+    document.querySelector(".left-hello").style.color = "#f52549";
+    document.querySelector(".main").style.color = "black";
+    document.querySelector(".quote").style.color = "black";
+    document.querySelector("hr").style.backgroundColor = "#2f3c7e";
+    document.querySelector(".skills").style.color = "#2f3c7e";
+    document.querySelector(".headingProjects").style.color = "#2f3c7e";
+    document.querySelector("footer").style.backgroundColor = "#2f3c7e";
+    document.querySelector(".outer-button").innerHTML = "Light Mode";
+    currMode = "light";
+    document.querySelector(".light1").innerHTML = "Light Mode";
+  }
+});
+
+
 
 
